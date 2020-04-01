17ab601ac756eebff9d6023c5aa6f75add0b6a54
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-01 12:29:35 -0400

    test text README file, add example code to app.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game-of-fetches
+## axios exercise
--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+
+    axios.get("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    .then(res => {
+      const recipes = res.data.results;
+      console.log("recipes ---->", recipes)
+      
+      this.setState({data: recipes})
+
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1> Spoonacular API </h1>
+        <ul>
+          {this.state.data.map((recipe, id) => <li key={id}> {recipe.title} </li>)}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
