9615cc18b4b8e575c2762c6dbed46bd93b311bd2
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-09 18:16:22 -0400

    succefull render of 1st question

--- game-of-thrones/src/components/Character.js
+++ game-of-thrones/src/components/Character.js
@@ -5,20 +5,21 @@ import axios  from 'axios';
 class Character extends React.Component{
     constructor(props){
         super(props)
-        this.state={ans1: '',imgUrl:''}
+        this.state={ans1: '',born:'',ans2:''}
     }
     async componentDidMount(){
-        const url= `https://anapioficeandfire.com/api/characters/583`;
+        const url= `https://anapioficeandfire.com/api/characters/16`;
         let response = await axios.get(url);
         let data= response.data;
         console.log(data);
-        this.setState({ans1: data.name,imgUrl: data.avatar_url})
+        this.setState({ans1: data.name,born: data.born})
     }
     render(){
         return (
             <div>
-                <h1>question</h1>
-                <h1>{this.state.ans1 }</h1>
+                <h1>Where was Margaery Tyrell born?</h1>
+                {/* <h2>{this.state.ans1}</h2> */}
+                <h2>{this.state.born}</h2>
             </div>
         )
     }
